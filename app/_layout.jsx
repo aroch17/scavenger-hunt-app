e15d221621af1558d0077bb27debf3ef5a40bbe5
Ayush <86850534+aroch17@users.noreply.github.com>
2024-04-19 15:37:13 +0100

    Populate index.jsx

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,14 +1,44 @@
+import { useEffect } from "react";
+import { View, Text } from "react-native";
+import { useFonts } from "expo-font";
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SplashScreen, Stack } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
+	const [fontsLoaded, error] = useFonts({
+		"Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+		"Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+		"Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+		"Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+		"Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+		"Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+		"Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+		"Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+		"Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+	});
+
+	useEffect(() => {
+		if (error) throw error;
+
+		if (fontsLoaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [fontsLoaded, error]);
+
+	if (!fontsLoaded) {
+		return null;
+	}
+
+	if (!fontsLoaded && !error) {
+		return null;
+	}
+
 	return (
-		<SafeAreaView>
-			<View className="bg-black h-full flex justify-center items-center">
-				<Text className="text-white text-3xl">Scavenger Hunt</Text>
-				<StatusBar style="auto" />
-			</View>
-		</SafeAreaView>
+		<Stack>
+			<Stack.Screen name="index" options={{ headerShown: false}}/>
+			<Stack.Screen name="(host-view)" options={{ headerShown: false }} />
+		</Stack>
 	);
 }
---
+++ app/index.jsx
@@ -0,0 +1,42 @@
+import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import React from "react";
+import { Redirect, router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Home = () => {
+	return (
+		<SafeAreaView className="bg-black">
+			<ScrollView contentContainerStyle={{height: '100%'}}>
+				<View className="bg-black h-full flex justify-center items-center">
+					<Text className="text-white text-3xl font-pbold mb-4">
+						Scavenger Hunt
+					</Text>
+					<View className="flex flex-row gap-3">
+						<TouchableOpacity
+							onPress={() => {
+								router.push("/host");
+							}}
+							activeOpacity={0.7}
+							className="bg-white p-[10px]"
+						>
+							<Text className="font-pregular">I am a host</Text>
+						</TouchableOpacity>
+						<TouchableOpacity
+							onPress={() => {
+								router.push("/guest");
+							}}
+							activeOpacity={0.7}
+							className="bg-white p-[10px]"
+						>
+							<Text className="font-pregular">I am a guest</Text>
+						</TouchableOpacity>
+					</View>
+				</View>
+			</ScrollView>
+			<StatusBar style="dark" />
+		</SafeAreaView>
+	);
+};
+
+export default Home;
