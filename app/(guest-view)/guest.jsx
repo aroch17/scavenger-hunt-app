1ec21d145eeda7a3485d608c29b079abbb41f0b8
Ayush <86850534+aroch17@users.noreply.github.com>
2024-04-27 19:24:01 +0100

    Reset host and guest files

--- app/(guest-view)/guest.jsx
+++ app/(guest-view)/guest.jsx
@@ -1,15 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from "react-native-safe-area-context";
 
 const Guest = () => {
-  return (
-    <SafeAreaView className="bg-black h-full">
-			<View>
-				<Text className="text-white">Guest</Text>
-			</View>
-		</SafeAreaView>
-  )
+	return (
+		<View>
+			<Text>Guest</Text>
+		</View>
+	)
 }
 
 export default Guest
