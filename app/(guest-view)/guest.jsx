7f3000be31ae925c0c993210ecf5f80186618351
Al1ceG <94125456+Al1ceG@users.noreply.github.com>
2024-05-02 22:26:05 +0100

    add view to guest.jsx file

--- app/(guest-view)/guest.jsx
+++ app/(guest-view)/guest.jsx
@@ -1,13 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Guest = () => {
   return (
     <SafeAreaView className="bg-black h-full">
-			<View>
-				<Text className="text-white">Guest</Text>
-			</View>
+			<ScrollView>
+				<View className="w-full justify-center min-h-[85vh] px-4 my-6">
+					<Text className="text-white">Guest</Text>
+					
+				</View>
+			</ScrollView>
 		</SafeAreaView>
   )
 }
