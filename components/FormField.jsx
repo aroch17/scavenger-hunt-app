06a70ab2b9cd523f1c2de14a356df27970266e36
Al1ceG <94125456+Al1ceG@users.noreply.github.com>
2024-05-02 22:41:13 +0100

    deal with input parameters in formfield.jsx

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 
-const FormField = () => {
+const FormField = ({title, value, placeholder, handleChangeText, otherStyles, ...props }) => {
   return (
-    <View className="space-y-2">
-      <Text classname= "text-base text-gray-100 font-pmedium">FormField</Text>
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text classname= "text-base text-gray-100 font-pmedium">{title}</Text>
+      <View className="border-3 border-red-500 w-full h-16 px-4 bg-black-100 rounded-2xl">
+        <TextInput className = "flex-1 text-white font-psemibold text-base"
+        value={value}
+        placeholder={placeholder}
+        placeholderTextColor="#7b7b8b"
+        onChangeText= {handleChangeText}>
+        </TextInput>
+      </View>
     </View>
   )
 }
