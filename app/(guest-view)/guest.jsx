27329037de57b0e78a160c58e78b0d0f20194c70
Al1ceG <94125456+Al1ceG@users.noreply.github.com>
2024-05-02 22:33:45 +0100

    Add formfield information and parameters to guest.jsx

--- app/(guest-view)/guest.jsx
+++ app/(guest-view)/guest.jsx
@@ -4,12 +4,23 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import FormField from '../../components/FormField';
 
 const Guest = () => {
+	const [form, setForm] = useState({
+		answer: ''
+	})
+
+
   return (
     <SafeAreaView className="bg-black h-full">
 			<ScrollView>
 				<View className="w-full justify-center min-h-[85vh] px-4 my-6">
 					<Text className="text-white">Guest</Text>
-					<FormField />
+					<FormField 
+						title="Answer"
+						value={form.answer}
+						handleChangeText ={(e) => setForm({ ...form, 
+						email: e})}
+						otherStyles="mt-7"
+					/>
 				</View>
 			</ScrollView>
 		</SafeAreaView>
