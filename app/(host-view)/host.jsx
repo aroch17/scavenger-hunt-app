8b32b5139b7deb0dfddc51482d0268488acc64be
Ayush <86850534+aroch17@users.noreply.github.com>
2024-05-03 16:39:58 +0100

    Add add question functionality

--- app/(host-view)/host.jsx
+++ app/(host-view)/host.jsx
@@ -1,15 +1,81 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { useMutation } from "@tanstack/react-query";
+import { addQuestion } from "../../lib/supabase";
+import uuid from 'react-native-uuid';
 
 const Host = () => {
+	function getCurrentDateTime() {
+		const now = new Date();
+
+		const year = now.getFullYear();
+		const month = String(now.getMonth() + 1).padStart(2, "0");
+		const day = String(now.getDate()).padStart(2, "0");
+		const hours = String(now.getHours()).padStart(2, "0");
+		const minutes = String(now.getMinutes()).padStart(2, "0");
+		const seconds = String(now.getSeconds()).padStart(2, "0");
+		const timezoneOffset = -now.getTimezoneOffset() / 60;
+
+		const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}${
+			timezoneOffset < 0 ? "-" : "+"
+		}${Math.abs(timezoneOffset).toString().padStart(2, "0")}`;
+
+		return formattedDateTime;
+	}
+
+	const mutation = useMutation({
+		mutationFn: addQuestion,
+		onSuccess: () => {
+			// Invalidate and refetch
+			queryClient.invalidateQueries({ queryKey: ["questions"] });
+		},
+	});
+
+	const [form, setForm] = useState({
+		question: "",
+		answer: "",
+	});
+	// console.log(form);
 	return (
 		<SafeAreaView className="bg-black h-full">
 			<View className="bg-black h-full items-center">
-				<Text className="text-white font-pbold text-2xl mt-20">Add Question</Text>
+				<Text className="text-white font-pbold text-2xl mt-20">
+					Add Question
+				</Text>
+				<FormField
+					title="Question"
+					handleChangeText={(e) => setForm({ ...form, question: e })}
+				/>
+				<FormField
+					title="Answer"
+					handleChangeText={(e) => setForm({ ...form, answer: e })}
+				/>
+				<CustomButton
+					title="Add"
+					containerStyles="mt-7 border-2 border-white w-full"
+					handlePress={() => {
+						const date = new Date();
+						let day = date.getDate();
+						let month = date.getMonth() + 1;
+						let year = date.getFullYear();
+						let time;
+						mutation.mutate({
+							id: uuid.v4(),
+							data: {
+								question: form.question,
+								answer: form.answer,
+							},
+							inserted_at: getCurrentDateTime(),
+							updated_at: getCurrentDateTime(),
+						});
+					}}
+				/>
 			</View>
-			<StatusBar style="light"/>
+			<StatusBar style="light" />
 		</SafeAreaView>
 	);
 };
