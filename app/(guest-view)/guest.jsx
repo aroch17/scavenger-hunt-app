bf7983d6480154483fe19d7c270b1b828cc474e6
Ayush <86850534+aroch17@users.noreply.github.com>
2024-05-03 13:51:17 +0100

    Add titles for host and guest pages

--- app/(guest-view)/guest.jsx
+++ app/(guest-view)/guest.jsx
@@ -1,11 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 const Guest = () => {
 	return (
-		<View>
-			<Text>Guest</Text>
-		</View>
+		<SafeAreaView className="bg-black h-full">
+			<View className="bg-black h-full items-center">
+				<Text className="text-white text-2xl font-pbold mt-20">Available Questions: </Text>
+			</View>
+			<StatusBar style="light"/>
+		</SafeAreaView>
 	)
 }
 
--- app/(host-view)/host.jsx
+++ app/(host-view)/host.jsx
@@ -1,12 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
 
 const Host = () => {
 	return (
-		<View>
-			<Text>Host</Text>
-		</View>
-	)
-}
+		<SafeAreaView className="bg-black h-full">
+			<View className="bg-black h-full items-center">
+				<Text className="text-white font-pbold text-2xl mt-20">Add Question</Text>
+			</View>
+			<StatusBar style="light"/>
+		</SafeAreaView>
+	);
+};
 
-export default Host
+export default Host;
