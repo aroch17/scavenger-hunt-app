e5f5ee93ec68e3c21f8ccd67837a51a72a77e135
Ayush <86850534+aroch17@users.noreply.github.com>
2024-05-03 19:41:07 +0100

    Delete unnecessary functions in host

--- app/(host-view)/host.jsx
+++ app/(host-view)/host.jsx
@@ -6,27 +6,8 @@ import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
 import { useMutation } from "@tanstack/react-query";
 import { addQuestion } from "../../lib/supabase";
-import uuid from 'react-native-uuid';
 
 const Host = () => {
-	function getCurrentDateTime() {
-		const now = new Date();
-
-		const year = now.getFullYear();
-		const month = String(now.getMonth() + 1).padStart(2, "0");
-		const day = String(now.getDate()).padStart(2, "0");
-		const hours = String(now.getHours()).padStart(2, "0");
-		const minutes = String(now.getMinutes()).padStart(2, "0");
-		const seconds = String(now.getSeconds()).padStart(2, "0");
-		const timezoneOffset = -now.getTimezoneOffset() / 60;
-
-		const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}${
-			timezoneOffset < 0 ? "-" : "+"
-		}${Math.abs(timezoneOffset).toString().padStart(2, "0")}`;
-
-		return formattedDateTime;
-	}
-
 	const mutation = useMutation({
 		mutationFn: addQuestion,
 		onSuccess: () => {
@@ -39,7 +20,7 @@ const Host = () => {
 		question: "",
 		answer: "",
 	});
-	// console.log(form);
+
 	return (
 		<SafeAreaView className="bg-black h-full">
 			<View className="bg-black h-full items-center">
@@ -58,19 +39,11 @@ const Host = () => {
 					title="Add"
 					containerStyles="mt-7 border-2 border-white w-full"
 					handlePress={() => {
-						const date = new Date();
-						let day = date.getDate();
-						let month = date.getMonth() + 1;
-						let year = date.getFullYear();
-						let time;
 						mutation.mutate({
-							id: uuid.v4(),
 							data: {
 								question: form.question,
 								answer: form.answer,
 							},
-							inserted_at: getCurrentDateTime(),
-							updated_at: getCurrentDateTime(),
 						});
 					}}
 				/>
