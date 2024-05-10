1259560cd7af627fbaba1dfe7dc5d9a2ad67976c
Al1ceG <94125456+Al1ceG@users.noreply.github.com>
2024-05-10 13:19:33 +0100

    combine task-viewer and task screen functionalities

--- app/(guest-view)/tasks.jsx
+++ app/(guest-view)/tasks.jsx
@@ -3,6 +3,8 @@ import {React, useState} from 'react'
 import { SafeAreaView } from "react-native-safe-area-context";
 import CustomButton from '../../components/CustomButton';
 import Task from '../../components/Task';
+import { useQuery } from "@tanstack/react-query";
+import { getQuestions } from "../../lib/supabase";
 
 const tasks = () => {
   const [isSubmitting, setSubmitting] = useState(false);
@@ -14,40 +16,39 @@ const tasks = () => {
 			Alert.alert("Answer was: " + form.answer)
 		}
 	}
+  const {
+		data: queryData,
+		isLoading,
+		error,
+	} = useQuery({ queryKey: ["questions"], queryFn: getQuestions });
 
   return (
+    <>
+    {!isLoading && (
     <SafeAreaView className="bg-black h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
       <View className="w-full justify-center min-h-[85vh] px-4 my-6">
 					<Text className="text-3xl font-semibold text-white mt-10 font-psemibold w-full text-center">Guest</Text>
 
-          <Task
-            title="Task 1"
-            taskType="Text"
-            handlePress={submit}
-            containerStyles="mt-7 border-2 border-white"
-            isLoading={isSubmitting}
-          />
+          {queryData.data.map((qa) => {
+            return (
+              <Task
+                key={qa.id}
+                title={qa.data.question}
+                handlePress={submit}
+                containerStyles="mt-7 border-2 border-white"
+                isLoading={isSubmitting}
+                taskType="Text"
+              >
 
-          <Task
-            title="Task 2"
-            taskType="Text"
-            handlePress={submit}
-            containerStyles="mt-7 border-2 border-white"
-            isLoading={isSubmitting}
-          />
-
-          <Task
-            title="Task 3"
-            handlePress={submit}
-            containerStyles="mt-7 border-2 border-white"
-            isLoading={isSubmitting}
-          />
-
-
+              </Task>
+            );
+          })}
 				</View>
       </ScrollView>
 		</SafeAreaView>
+    )}
+    </>
   )
 }
 
