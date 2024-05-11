a2ed19d6a39e1d11b557c95cd4f7ae662884736e
Ayush <86850534+aroch17@users.noreply.github.com>
2024-05-11 03:00:07 +0100

    Remove header from task screen

--- app/task/[id].jsx
+++ app/task/[id].jsx
@@ -1,30 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import DisplayTask from '../../components/DisplayTask';
-import { useLocalSearchParams } from 'expo-router';
+import DisplayTask from "../../components/DisplayTask";
+import { useLocalSearchParams } from "expo-router";
+import { useQuery } from "@tanstack/react-query";
+import { getTask } from "../../lib/supabase";
 
 const viewTask = () => {
-  const task = useLocalSearchParams();
+	const taskId = useLocalSearchParams();
 
-  console.log(task)
+	const { data, isLoading, error } = useQuery({
+		queryKey: ["tasks"],
+		queryFn: () => getTask(taskId),
+	});
 
-  return (
-    <SafeAreaView className="bg-black h-full">
-			<View>
+	const task = data.data[0];
 
-				
-				<DisplayTask
-				title={task.title}
-				prompt={task.prompt}
-				answer={task.answer}
-				otherStyles="mt-7"
-				textStyles="text-base text-white font-pmedium"
-				taskType={task.task_type}
-				/>
-			</View>
-		</SafeAreaView>
-  )
-}
+	return (
+		<>
+			{!isLoading && (
+				<SafeAreaView className="bg-black h-full">
+					<ScrollView contentContainerStyle={{ height: "100%" }}>
+						<View>
+							<DisplayTask
+								title={task.title}
+								prompt={task.prompt}
+								answer={task.answer}
+								otherStyles="mt-7"
+								textStyles="text-base text-white font-pmedium"
+								taskType={task.task_type}
+							/>
+						</View>
+					</ScrollView>
+				</SafeAreaView>
+			)}
+		</>
+	);
+};
 
-export default viewTask
+export default viewTask;
