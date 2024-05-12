cd529fe0da7655c403e2117ad24039d4187bf2ed
Ayush <86850534+aroch17@users.noreply.github.com>
2024-05-12 03:15:44 +0100

    Display hunt component on host home

--- app/(host-view)/hunts.jsx
+++ app/(host-view)/hunts.jsx
@@ -4,31 +4,46 @@ import { useQuery } from "@tanstack/react-query";
 import { useGlobalContext } from "../../context/GlobalProvider";
 import { getHunts } from "../../lib/supabase";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Hunt from "../../components/Hunt";
+import { router } from "expo-router";
 
 const Hunts = () => {
 	const { user } = useGlobalContext();
 
-	const { data: queryData, isLoading, error } = useQuery({
+	const {
+		data: queryData,
+		isLoading,
+		error,
+	} = useQuery({
 		queryKey: ["userHunts"],
-		queryFn: async () => {
-			const { data, error } = await getHunts(user.id);
-			return { data, error };
-		},
+		queryFn: () => getHunts(user.id)
 	});
 
 	return (
 		<>
 			{!isLoading && (
 				<SafeAreaView className="bg-black h-full">
-					<ScrollView contentContainerStyle={{ height: "100%" }}>
+					<Text className="text-white text-2xl font-pbold mt-10">Your hunts:</Text>
+					<ScrollView>
 						<View className="w-full justify-center min-h-[85vh] px-4 my-6">
-            <Text className="text-white text-2xl font-pbold">Your hunts:</Text>
-							{queryData.data.map((hunt) => {
-								return (
-                  // TODO: Make hunt component and redirect to hunt page when clicked
-									<Text key={hunt.id} className="text-white for-pregular">{hunt.name}</Text>
-								);
-							})}
+							{queryData.data.length > 0 ? (
+								queryData.data.map((hunt) => {
+									return (
+										<Hunt
+											key={hunt.id}
+											name={hunt.name}
+											containerStyles="mt-7 border-2 border-white"
+											handlePress={() => {
+												router.push(`/hunt/${hunt.id}`);
+											}}
+										/>
+									);
+								})
+							) : (
+								<Text className="text-white font-pregular text-">
+									No hunts to display.
+								</Text>
+							)}
 						</View>
 					</ScrollView>
 				</SafeAreaView>
