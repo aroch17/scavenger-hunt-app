df7e3fff18eff5ea935f2b1729b7ebf0f56e8503
Ayush <86850534+aroch17@users.noreply.github.com>
2024-05-13 01:32:51 +0100

    Add add hunt button to hunts

--- app/(host-view)/hunts.jsx
+++ app/(host-view)/hunts.jsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useGlobalContext } from "../../context/GlobalProvider";
@@ -6,6 +6,7 @@ import { getHunts } from "../../lib/supabase";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Hunt from "../../components/Hunt";
 import { router } from "expo-router";
+import CustomButton from "../../components/CustomButton";
 
 const Hunts = () => {
 	const { user } = useGlobalContext();
@@ -16,36 +17,46 @@ const Hunts = () => {
 		error,
 	} = useQuery({
 		queryKey: ["userHunts"],
-		queryFn: () => getHunts(user.id)
+		queryFn: () => getHunts(user.id),
 	});
 
 	return (
 		<>
 			{!isLoading && (
 				<SafeAreaView className="bg-black h-full">
-					<Text className="text-white text-2xl font-pbold mt-10">Your hunts:</Text>
-					<ScrollView>
-						<View className="w-full justify-center min-h-[85vh] px-4 my-6">
-							{queryData.data.length > 0 ? (
-								queryData.data.map((hunt) => {
-									return (
-										<Hunt
-											key={hunt.id}
-											name={hunt.name}
-											containerStyles="mt-7 border-2 border-white"
-											handlePress={() => {
-												router.push(`/hunt/${hunt.id}`);
-											}}
-										/>
-									);
-								})
-							) : (
-								<Text className="text-white font-pregular text-">
-									No hunts to display.
-								</Text>
-							)}
-						</View>
-					</ScrollView>
+					<Text className="text-white text-2xl font-pbold mt-16">
+						Your hunts:
+					</Text>
+					<View className="w-full px-4 my-6">
+						{queryData.data.length > 0 ? (
+							<FlatList
+								className="max-h-[80%]"
+								data={queryData.data}
+								renderItem={({ item }) => (
+									<Hunt
+										key={item.id}
+										name={item.name}
+										containerStyles="mt-7 border-2 border-white"
+										handlePress={() => {
+											router.push(`/hunt/${item.id}`);
+										}}
+									/>
+								)}
+							/>
+						) : (
+							<Text className="text-white font-pregular text-xl">
+								No hunts to display.
+							</Text>
+						)}
+						<CustomButton
+							title="Add Hunt"
+							containerStyles="mt-7 bg-white w-full"
+							textStyles="text-black"
+							handlePress={() => {
+								router.push("/hunt-builder");
+							}}
+						/>
+					</View>
 				</SafeAreaView>
 			)}
 		</>
