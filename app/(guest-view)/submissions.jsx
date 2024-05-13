2774a759475463a8e5ccba4737484ea58bffeef8
Ayush <86850534+aroch17@users.noreply.github.com>
2024-05-13 14:56:38 +0100

    Make guest submissions scroll smoother

--- app/(guest-view)/submissions.jsx
+++ app/(guest-view)/submissions.jsx
@@ -1,48 +1,51 @@
-import { View, Text, ScrollView, Alert } from 'react-native'
+import { View, Text, ScrollView, Alert, FlatList } from "react-native";
 import { React, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useQuery } from "@tanstack/react-query";
 import { getSubmissions } from "../../lib/supabase";
 import Submission from "../../components/Submission";
 
-
 const submissions = () => {
-
 	const {
 		data: queryData,
 		isLoading,
 		error,
 	} = useQuery({ queryKey: ["submissions"], queryFn: getSubmissions });
 
-  return (
+	return (
 		<>
-		{!isLoading && (
-			<SafeAreaView className="bg-black h-full">
-				<ScrollView contentContainerStyle={{ height: "100%" }}>
-					<View className="w-full justify-center min-h-[85vh] px-4 my-6">
-						<Text className="text-3xl font-semibold text-white mt-10 font-psemibold w-full text-center">
-							Guest
-						</Text>
-
-						{queryData.data.map((submission) => {
-							return (
-								<Submission
-									key={submission.id}
-									submission={submission.submission}
-									task_id={submission.task_id}
-									created_at={submission.created_at}
-									user_id={submission.user_id}
-									containerStyles="mt-7 border-2 border-white"
-									textStyles="text-white"
-								/>
-							);
-						})}
+			{!isLoading && (
+				<SafeAreaView className="bg-black h-full">
+					<Text className="text-3xl font-semibold text-white mt-10 font-psemibold w-full text-center">
+						Your submissions:
+					</Text>
+					<View className="w-full px-4 my-6">
+						{queryData.data.length > 0 ? (
+							<FlatList
+								className="max-h-[95%]"
+								data={queryData.data}
+								renderItem={({ item }) => (
+									<Submission
+										key={item.id}
+										submission={item.submission}
+										task_id={item.task_id}
+										created_at={item.created_at}
+										user_id={item.user_id}
+										containerStyles="mt-7 border-2 border-white"
+										textStyles="text-white"
+									/>
+								)}
+							/>
+						) : (
+							<Text className="text-white font-pregular text-xl">
+								No hunts to display.
+							</Text>
+						)}
 					</View>
-				</ScrollView>
-			</SafeAreaView>
-		)}
-	</>
-  )
-}
+				</SafeAreaView>
+			)}
+		</>
+	);
+};
 
-export default submissions
+export default submissions;
