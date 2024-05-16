529c6cae424caf890cbd41666318b950091ca4ee
Ayush <86850534+aroch17@users.noreply.github.com>
2024-05-16 02:06:23 +0100

    Only show home screen on app open

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { useGlobalContext } from '../context/GlobalProvider'
 import Home from './home'
-import Hunts from './(host-view)/hunts'
-import Profile from './(host-view)/profile'
 
 const Index = () => {
-	const { isLoggedIn, isLoading } = useGlobalContext()
+	const { isLoading } = useGlobalContext()
 	return (
 		<>
-		{!isLoading && isLoggedIn ? <Profile /> : <Home />}
+		{!isLoading && <Home />}
 		</>
 	)
 }
