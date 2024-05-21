7cc164a71b863aa789ccd4c1a4f8d9dec5af1710
Ayush <86850534+aroch17@users.noreply.github.com>
2024-05-21 04:37:32 +0100

    Add logic to display hunt teams

--- app/hunt/[huntId]/teams.jsx
+++ app/hunt/[huntId]/teams.jsx
@@ -1,11 +1,46 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useLocalSearchParams, router } from "expo-router";
+import Task from "../../../components/Task";
+import CustomButton from "../../../components/CustomButton";
+import { useHuntContext } from "./_layout";
+
 
 const HuntTeams = () => {
+  const { hunt, isLoading } = useHuntContext()
+
   return (
-    <View>
-      <Text>HuntTeams</Text>
-    </View>
+    <>
+			{!isLoading && (
+				<SafeAreaView className="bg-black h-full">
+					<View className="bg-black items-center">
+						<Text className="text-white font-pbold text-2xl mt-20">
+							{hunt.name}
+						</Text>
+						<Text className="mt-5 font-bold text-white text-xl">Teams: </Text>
+						{hunt.teams.length > 0 ? (
+							<FlatList
+								className="max-h-[60%]"
+								data={hunt.teams}
+								renderItem={({ item }) => (
+									<Task
+										key={item.id}
+										title={item.name}
+										containerStyles="mt-7 border-2 border-white"
+									/>
+								)}
+							/>
+						) : (
+							<Text className="text-white font-pregular text-xl">
+								No teams added yet.
+							</Text>
+						)}
+            {/* should hosts be able to add teams? */}
+					</View>
+				</SafeAreaView>
+			)}
+		</>
   )
 }
 
