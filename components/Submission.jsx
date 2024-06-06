345c4b95637097e0e18e00cd76d42fc784af224d
Ayush <86850534+aroch17@users.noreply.github.com>
2024-06-06 14:09:44 +0100

    Allow hosts to view hunt submissions

--- components/Submission.jsx
+++ components/Submission.jsx
@@ -1,14 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const Submission = ({ submission, task_id, created_at, user_id, containerStyles, textStyles}) => {
+const Submission = ({ submission, task_id, created_at, team_id, containerStyles, textStyles}) => {
   return (
     <View>
       <View className={containerStyles}>
         <Text className={textStyles}>Answer: {submission}</Text>
         <Text className={textStyles}>TaskID: {task_id}</Text>
         <Text className={textStyles}>Created At: {created_at}</Text>
-        <Text className={textStyles}>User: {user_id}</Text>
+        <Text className={textStyles}>Team: {team_id}</Text>
 
       </View>
       
