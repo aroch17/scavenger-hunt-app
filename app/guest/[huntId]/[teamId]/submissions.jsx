94f5de9fb99b761797fbbacef827d096f9ac7ab3
Ayush <86850534+aroch17@users.noreply.github.com>
2024-06-06 14:08:46 +0100

    Remove unused function and fix submission box height

--- app/guest/[huntId]/[teamId]/submissions.jsx
+++ app/guest/[huntId]/[teamId]/submissions.jsx
@@ -1,17 +1,10 @@
 import { View, Text, ScrollView, Alert, FlatList } from "react-native";
 import { React, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useQuery } from "@tanstack/react-query";
-import { getSubmissions } from "../../../../lib/supabase";
 import Submission from "../../../../components/Submission";
 import { useTeamContext } from "./_layout";
 
 const submissions = () => {
-	// const {
-	// 	data: queryData,
-	// 	isLoading,
-	// 	error,
-	// } = useQuery({ queryKey: ["submissions"], queryFn: getSubmissions });
 
 	const { huntId, hunt, teamId, team, isLoading } = useTeamContext()
 
@@ -25,7 +18,7 @@ const submissions = () => {
 					<View className="w-full px-4 my-6">
 						{hunt.submissions.length > 0 ? (
 							<FlatList
-								className="max-h-[95%]"
+								className="min-h-[80%] max-h-[95%]"
 								data={hunt.submissions}
 								renderItem={({ item }) => (
 									<Submission
