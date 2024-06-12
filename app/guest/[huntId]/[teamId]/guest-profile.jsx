525320e5ce1a214656bc332a461798769d9b9166
Ayush <86850534+aroch17@users.noreply.github.com>
2024-06-12 05:14:01 +0100

    Enable real time updates for team submssions on guest side

--- app/guest/[huntId]/[teamId]/guest-profile.jsx
+++ app/guest/[huntId]/[teamId]/guest-profile.jsx
@@ -1,51 +1,68 @@
-import { View, Text, FlatList } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Ionicons } from '@expo/vector-icons';
-import { useTeamContext } from './_layout';
-import Submission from '../../../../components/Submission';
+import { useTeamContext } from "./_layout";
+import Submission from "../../../../components/Submission";
+import { getTeamSubmissions, supabase } from "../../../../lib/supabase";
 
 const profile = () => {
+	const { huntId, teamId, team, isLoading } = useTeamContext();
+	const [teamSubmissions, setTeamSubmissions] = useState(team.submissions)
 
-	const { huntId, hunt, teamId, team, isLoading } = useTeamContext()
+	useEffect(() => {
+		const channel = supabase
+			.channel("guest-team-submissions")
+			.on(
+				"postgres_changes",
+				{
+					event: "*",
+					schema: "public",
+					table: "submissions",
+				},
+				async (payload) => {
+					const data = await getTeamSubmissions(huntId, teamId);
+					setTeamSubmissions(data.data);
+					team.submissions = data.data;
+				}
+			)
+			.subscribe();
+	}, []);
 
-	const teamSubmissions = hunt.submissions.filter(submission => submission.team_id == teamId);
-
-  return (
+	return (
 		<>
-		{!isLoading && (
-			<SafeAreaView className="bg-black h-full">
-				<Text className="text-3xl font-semibold text-white mt-10 font-psemibold w-full text-center">
-					Your submissions:
-				</Text>
-				<View className="w-full px-4 my-6">
-					{teamSubmissions.length > 0 ? (
-						<FlatList
-							className="min-h-[80%] max-h-[95%]"
-							data={teamSubmissions}
-							renderItem={({ item }) => (
-								<Submission
-									key={item.id}
-									submission={item.submission}
-									task_id={item.task_id}
-									team_id = {item.team_id}
-									created_at={item.created_at}
-									user_id={item.user_id}
-									containerStyles="mt-7 border-2 border-white"
-									textStyles="text-white"
-								/>
-							)}
-						/>
-					) : (
-						<Text className="text-white font-pregular text-xl">
-							No submissions to display.
-						</Text>
-					)}
-				</View>
-			</SafeAreaView>
-		)}
-	</>
-  )
-}
+			{!isLoading && (
+				<SafeAreaView className="bg-black h-full">
+					<Text className="text-3xl font-semibold text-white mt-10 font-psemibold w-full text-center">
+						Your submissions:
+					</Text>
+					<View className="w-full px-4 my-6">
+						{teamSubmissions.length > 0 ? (
+							<FlatList
+								className="min-h-[80%] max-h-[95%]"
+								data={teamSubmissions}
+								renderItem={({ item }) => (
+									<Submission
+										key={item.id}
+										submission={item.submission}
+										task_id={item.task_id}
+										team_id={item.team_id}
+										created_at={item.created_at}
+										user_id={item.user_id}
+										containerStyles="mt-7 border-2 border-white"
+										textStyles="text-white"
+									/>
+								)}
+							/>
+						) : (
+							<Text className="text-white font-pregular text-xl">
+								No submissions to display.
+							</Text>
+						)}
+					</View>
+				</SafeAreaView>
+			)}
+		</>
+	);
+};
 
-export default profile
+export default profile;
