f64cf53fd510318a1a6f2d608f8c8d86155fc608
Ayush <86850534+aroch17@users.noreply.github.com>
2024-06-12 03:54:31 +0100

    Add todo

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -6,6 +6,7 @@ const GlobalContext = createContext();
 
 export const useGlobalContext = () => useContext(GlobalContext)
 
+// TODO: Add a way to store whether user has joined a hunt
 export const GlobalProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [user, setUser] = useState(null)
