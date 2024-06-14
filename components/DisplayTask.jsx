f3dc360906b20026318ff3b3e41a508679910500
Ayush <86850534+aroch17@users.noreply.github.com>
2024-06-14 11:02:09 +0100

    Add upload image component to display task

--- components/DisplayTask.jsx
+++ components/DisplayTask.jsx
@@ -1,20 +1,31 @@
-import {Text, Image, View, TextInput, Alert, ScrollView} from 'react-native'
-import {React, useState} from 'react'
-import { icons } from "../constants";
-import FormField from './FormField';
-import CustomButton from './CustomButton';
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, View, Alert } from "react-native";
+import { React, useState } from "react";
+import FormField from "./FormField";
+import CustomButton from "./CustomButton";
 import { useMutation } from "@tanstack/react-query";
 import { addSubmission } from "../lib/supabase";
+import UploadImage from "./UploadImageButton";
 
-const DisplayTask = ({ taskId, teamId, title, prompt, answer, taskType, huntId, otherStyles, textStyles, value, placeholder, handleChangeText}) => {
-
-  const [isSubmitting, setSubmitting] = useState(false);
+const DisplayTask = ({
+	taskId,
+	teamId,
+	title,
+	prompt,
+	answer,
+	taskType,
+	huntId,
+	otherStyles,
+	textStyles,
+	value,
+	placeholder,
+	handleChangeText,
+}) => {
+	const [isSubmitting, setSubmitting] = useState(false);
 	const [form, setForm] = useState({
-		answer: '',
+		answer: "",
 	});
 
-  const mutation = useMutation({
+	const mutation = useMutation({
 		mutationFn: addSubmission,
 		onSuccess: () => {
 			// Invalidate and refetch
@@ -23,47 +34,52 @@ const DisplayTask = ({ taskId, teamId, title, prompt, answer, taskType, huntId, 
 	});
 
 	const submit = async () => {
-    if (form.answer === "") {
-      Alert.alert("Error", "Please fill in all fields");
-    }
-		else if (form.answer === answer){
-			Alert.alert("Answer was correct: " + form.answer)
-      mutation.mutate({
-        submission: form.answer,
-        task_id: taskId,
-        team_id: teamId,
-        hunt_id: huntId
-      });
-
+		if (form.answer === "") {
+			Alert.alert("Error", "Please fill in all fields");
+		} else if (form.answer === answer) {
+			Alert.alert("Answer was correct: " + form.answer);
+			mutation.mutate({
+				submission: form.answer,
+				task_id: taskId,
+				team_id: teamId,
+				hunt_id: huntId,
+			});
+		} else {
+			Alert.alert("Incorrect");
 		}
-    else{
-      Alert.alert("Incorrec t")
-    }
-  }
+	};
 
-  return (
-    <View className="w-full justify-center min-h-[85vh] px-4 my-6">
-      <Text className={textStyles}>{title}</Text>
-      <Text className={textStyles}>{prompt}</Text>
-      <Text className={textStyles}>Type: {taskType}</Text>
+	return (
+		<View className="w-full justify-center min-h-[85vh] px-4 my-6">
+			<Text className={textStyles}>{title}</Text>
+			<Text className={textStyles}>{prompt}</Text>
+			<Text className={textStyles}>Type: {taskType}</Text>
 
-      <FormField
-        title="Put answer here: "
-        value={form.answer}
-        handleChangeText ={(e) => setForm({ ...form, 
-        answer: e})}
-        otherStyles="mt-7"
-      />
+			{taskType == "Text" ? (
+				<>
+					<FormField
+						title="Put answer here: "
+						value={form.answer}
+						handleChangeText={(e) => setForm({ ...form, answer: e })}
+						otherStyles="mt-7"
+					/>
 
-      <CustomButton
-        title="Submit"
-        handlePress={submit}
-        containerStyles="mt-7 border-2 border-white"
-        isLoading={isSubmitting}
-      />
+					<CustomButton
+						title="Submit"
+						handlePress={submit}
+						containerStyles="mt-7 border-2 border-white"
+						isLoading={isSubmitting}
+					/>
+				</>
+			) : taskType == "Image" ? (
+				<>
+					<UploadImage huntId={huntId} teamId={teamId}/>
+				</>
+			) : (
+        <></>
+      )}
+		</View>
+	);
+};
 
-    </View>
-  )
-}
-
-export default DisplayTask
+export default DisplayTask;
