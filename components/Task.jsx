0ddb05f79c2424308b9809ed4569572c8f27a7e5
Ayush <86850534+aroch17@users.noreply.github.com>
2024-06-15 09:11:22 +0100

    Add icon for image type task

--- components/Task.jsx
+++ components/Task.jsx
@@ -1,37 +1,41 @@
-import { TouchableOpacity, Text, Image, View } from 'react-native'
-import React from 'react'
-import { icons } from "../constants";
-import { Ionicons } from '@expo/vector-icons';
+import { TouchableOpacity, Text, View } from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 
-const Task = ({ title, taskType, handlePress, containerStyles, textStyles, isLoading }) => {
+const Task = ({
+	title,
+	taskType,
+	handlePress,
+	containerStyles,
+	textStyles,
+	isLoading,
+}) => {
+	return (
+		<TouchableOpacity
+			onPress={handlePress}
+			activeOpacity={0.7}
+			className={`border-white rounded-xl px-4 min-h-[62px] w-full items-center flex flex-row ${containerStyles} $ {isLoading ? 'opacity-50' : ''}`}
+			disabled={isLoading}
+		>
+			{taskType === "Text" ? (
+				<View className="absolute mx-4">
+					<Ionicons name="text" size={20} color="gray" />
+				</View>
+			) : taskType == "Image" ? (
+				<View className="absolute mx-4">
+					<Ionicons name="camera-outline" size={20} color="gray" />
+				</View>
+			) : (
+				<></>
+			)}
 
-  return (
+			<View className="flex flex-row w-full justify-center">
+				<Text className={`text-white font-psemibold text-lg" ${textStyles}`}>
+					{title}
+				</Text>
+			</View>
+		</TouchableOpacity>
+	);
+};
 
-    <TouchableOpacity
-      onPress={handlePress}
-      activeOpacity={0.7}
-      className={`border-white rounded-xl px-4 min-h-[62px] w-full items-center flex flex-row ${containerStyles} $ {isLoading ? 'opacity-50' : ''}`}
-      disabled={isLoading}
-    >
-
-      {taskType === "Text" && (
-        <View className="absolute mx-4">
-          <Ionicons
-            name="text" 
-            size={20} 
-            color="gray"
-          />
-        </View>
-      )}
-
-
-      <View className="flex flex-row w-full justify-center">
-        <Text className={`text-white font-psemibold text-lg" ${textStyles}`}>
-          {title}
-        </Text>
-      </View>
-    </TouchableOpacity>
-  )
-}
-
-export default Task
+export default Task;
