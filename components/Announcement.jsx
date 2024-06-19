a8338a9993e6e48f3664a1049fb0597b057dfc6f
Ayush <86850534+aroch17@users.noreply.github.com>
2024-06-19 19:53:29 +0100

    Fix edge case in announcement component

--- components/Announcement.jsx
+++ components/Announcement.jsx
@@ -9,7 +9,7 @@ const Announcement = ({ title, description, containerStyles, textStyles }) => {
     <View>
       <View className={`rounded-xl w-full justify-between items-center min-h-[40px] px-10 py-3 ${containerStyles}`}>
       <Text className={`text-[18em] text-white font-psemibold ${textStyles}`}>{title}</Text>
-      {description.length > 0 && (
+      {description && (
         <Text className={`text-white font-pregular textStyles`}>{description}</Text>
       )}
 
