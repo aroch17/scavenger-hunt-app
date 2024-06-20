a020558b762d275eb4075d21f0f7c83aee979a88
Ayush <86850534+aroch17@users.noreply.github.com>
2024-06-20 08:58:41 +0100

    Add toggle password button

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,26 +1,65 @@
-import { View, Text, TextInput } from 'react-native'
-import {React, useState} from 'react'
+import { View, Text, TextInput } from "react-native";
+import { React, useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 const FormField = ({
-  title, 
-  value, 
-  placeholder, 
-  handleChangeText, 
-  otherStyles, 
+	title,
+	value,
+	isPasswordField,
+	placeholder,
+	handleChangeText,
+	otherStyles,
 }) => {
-  return (
-    <View className={`space-y-2 ${otherStyles}`}>
-      <Text className= "text-base text-white font-pmedium">{title}</Text>
-      <View className="w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary flex flex-row items-center">
-        <TextInput className = "flex-1 text-white font-psemibold text-base"
-        value={value}
-        placeholder={placeholder}
-        placeholderTextColor="#7B7B8B"
-        onChangeText= {handleChangeText}
-        />
-      </View>
-    </View>
-  )
-}
+	const [textHidden, setTextHidden] = useState(isPasswordField);
+	return (
+		<View className={`space-y-2 ${otherStyles}`}>
+			<Text className="text-base text-white font-pmedium">{title}</Text>
+			<View className="w-full h-16 px-5 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary flex flex-row items-center">
+				{isPasswordField ? (
+					<>
+						<TextInput
+							className="flex-1 text-white font-psemibold text-base"
+							value={value}
+							placeholder={placeholder}
+							placeholderTextColor="#7B7B8B"
+							onChangeText={handleChangeText}
+							secureTextEntry={textHidden}
+						/>
+            {textHidden ? (
+              <Ionicons
+							color={"white"}
+							name="eye-outline"
+							size={25}
+							onPress={() => {
+								setTextHidden(!textHidden)
+							}}
+						/>
+            ) : (
+              <Ionicons
+							color={"white"}
+							name="eye-off-outline"
+							size={25}
+							onPress={() => {
+								setTextHidden(!textHidden)
+							}}
+						/>
+            )}
+						
+					</>
+				) : (
+					<>
+						<TextInput
+							className="flex-1 text-white font-psemibold text-base"
+							value={value}
+							placeholder={placeholder}
+							placeholderTextColor="#7B7B8B"
+							onChangeText={handleChangeText}
+						/>
+					</>
+				)}
+			</View>
+		</View>
+	);
+};
 
-export default FormField
+export default FormField;
