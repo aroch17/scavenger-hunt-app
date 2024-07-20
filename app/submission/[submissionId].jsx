257814826b0bced637bac0fd16fa6606b5f1f592
Ayush <86850534+aroch17@users.noreply.github.com>
2024-07-20 07:19:47 +0100

    Enable hosts to approve/reject photos

--- app/submission/[submissionId].jsx
+++ app/submission/[submissionId].jsx
@@ -1,14 +1,19 @@
 import { View, Text, ScrollView, Image } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import { useQuery } from "@tanstack/react-query";
-import { getSubmission } from "../../lib/supabase";
+import {
+	changeSubmissionStatus,
+	getSubmission,
+	updateTeamScore,
+} from "../../lib/supabase";
+import CustomButton from "../../components/CustomButton";
 
 const viewSubmission = () => {
-	const CDNUrl = "https://psdsdmptskceretwhxxt.supabase.co/storage/v1/object/public/submissions"
+	const CDNUrl =
+		"https://psdsdmptskceretwhxxt.supabase.co/storage/v1/object/public/submissions";
 	const { submissionId } = useLocalSearchParams();
-	// TODO: Show image submission to host
 	let submission = null;
 
 	const { data, isLoading, error } = useQuery({
@@ -19,7 +24,6 @@ const viewSubmission = () => {
 	if (!isLoading && !error) {
 		submission = data.data[0];
 	}
-	// TODO: Enable host to approve/reject submission
 	return (
 		<>
 			{!isLoading && (
@@ -29,7 +33,9 @@ const viewSubmission = () => {
 							<Text className="text-white text-2xl font-pbold">
 								{submission.tasks.title}
 							</Text>
-							<Text className="text-white font-pregular text-xl">{submission.tasks.prompt}</Text>
+							<Text className="text-white font-pregular text-xl">
+								{submission.tasks.prompt}
+							</Text>
 							<Text className="text-white text-xl">Submission:</Text>
 							{submission.image_uuid ? (
 								<Image
@@ -44,7 +50,48 @@ const viewSubmission = () => {
 									}}
 								/>
 							) : (
-								<Text className="text-white font-pregular text-xl">{submission.tasks.answer}</Text>
+								<Text className="text-white font-pregular text-xl">
+									{submission.tasks.answer}
+								</Text>
+							)}
+							{submission.requires_approval ? (
+								<View>
+									<CustomButton
+										title="Approve"
+										containerStyles="mt-7 border-2 border-green-600 w-full"
+										handlePress={async () => {
+											await changeSubmissionStatus(submission.id, true);
+											await updateTeamScore({
+												team_id: submission.team_id,
+												points: submission.tasks.points,
+											});
+											router.push("../");
+										}}
+									/>
+									<CustomButton
+										title="Reject"
+										containerStyles="mt-2 border-2 border-red-600 w-full"
+										handlePress={async () => {
+											await changeSubmissionStatus(submission.id, false);
+											router.push("../");
+										}}
+									/>
+								</View>
+							) : (
+								<View>
+									<Text className="text-white font-pbold text-xl">
+										Status:{" "}
+									</Text>
+									{submission.is_approved ? (
+										<Text className="text-green-600 font-pregular text-xl">
+											Approved
+										</Text>
+									) : (
+										<Text className="text-red-600 font-pregular text-xl">
+											Rejected
+										</Text>
+									)}
+								</View>
 							)}
 						</View>
 					</ScrollView>
