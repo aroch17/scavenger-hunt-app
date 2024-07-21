eb66ca5a6c5c1ec40029fd88d768cc591096463a
Ayush <86850534+aroch17@users.noreply.github.com>
2024-07-21 13:54:06 +0100

    Add toggle for displaying completed tasks

--- app/guest/[huntId]/[teamId]/tasks.jsx
+++ app/guest/[huntId]/[teamId]/tasks.jsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, Alert, FlatList } from "react-native";
+import { View, Text, FlatList, Switch } from "react-native";
 import { React, useState, useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Task from "../../../../components/Task";
@@ -9,7 +9,9 @@ import { useTeamContext } from "./_layout";
 const tasks = () => {
 	const { huntId, hunt, teamId, team, isLoading } = useTeamContext();
 
-	const [isSubmitting, setSubmitting] = useState(false);
+	const [isEnabled, setIsEnabled] = useState(false);
+	const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
 	const [tasks, setTasks] = useState(hunt.tasks);
 	const select = (task) => {
 		router.push(`/task/${teamId}/${task.id}`);
@@ -40,6 +42,18 @@ const tasks = () => {
 						<Text className="text-3xl font-semibold text-white mt-10 font-psemibold w-full text-center">
 							Tasks:
 						</Text>
+						<View className="flex flex-row justify-center mt-5">
+							<Text className="text-white text-2xl font-pregular">
+								Show Completed:{" "}
+							</Text>
+							<Switch
+								trackColor={{ false: "#767577", true: "#81b0ff" }}
+								thumbColor={isEnabled ? "#f4f3f4" : "#f4f3f4"}
+								ios_backgroundColor="#3e3e3e"
+								onValueChange={toggleSwitch}
+								value={isEnabled}
+							/>
+						</View>
 						{tasks.length > 0 ? (
 							<FlatList
 								className="min-h-[80%] max-h-[95%]"
@@ -50,7 +64,6 @@ const tasks = () => {
 										title={item.title}
 										handlePress={() => select(item)}
 										containerStyles="mt-7 border-2 border-white"
-										isLoading={isSubmitting}
 										taskType={item.task_type}
 									/>
 								)}
